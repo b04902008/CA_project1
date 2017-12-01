4dd59cf4220a856eebb206b5da6fd6662a97504e
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-12-01 17:04:30 +0800

    fix data 8bit/32bit

--- project1_src/Data_Memory.v
+++ project1_src/Data_Memory.v
@@ -22,13 +22,16 @@ reg     [7:0]     memory  [0:31];
 // Write Data   
 always@(posedge clk_i) begin
     if (MemWrite_i)
-        memory[addr_i] <= data_i;
+        memory[addr_i] <= data_i[7:0];
+        memory[addr_i+1] <= data_i[15:8];
+        memory[addr_i+2] <= data_i[23:16];
+        memory[addr_i+3] <= data_i[31:24];
 end
    
 // Read Data  
 always@(*) begin
     if (MemRead_i)
-        data_o <= memory[addr_i] ;
+        data_o <= {memory[addr_i+3], memory[addr_i+2], memory[addr_i+1], memory[addr_i]} ;
 end
 
 endmodule 
