d3cf40d4e3c67a9d8892caf9e12d126ffd98a55f
alex <a1s2d3198@gmail.com>
2017-12-01 17:06:57 +0800

    fix Register.RDdata_i & MUX_RegSrc.data{1,2}_i

--- project1_src/CPU.v
+++ project1_src/CPU.v
@@ -64,7 +64,7 @@ Registers Registers(
     .RSaddr_i   (inst[25:21]),
     .RTaddr_i   (inst[20:16]),
     .RDaddr_i   (MUX_RegDst.data_o),
-    .RDdata_i   (ALU.data_o),
+    .RDdata_i   (MUX_RegSrc.data_o),
     .RegWrite_i (Control.RegWrite_o),
     .RSdata_o   (),//ALU.data1_i
     .RTdata_o   ()//MUX_ALUSrc.data1_i
@@ -85,8 +85,8 @@ MUX32 MUX_ALUSrc(
 );
 
 MUX32 MUX_RegSrc(
-    .data1_i    (Data_Memory.data_o),
-    .data2_i    (ALU.data_o),
+    .data1_i    (ALU.data_o),
+    .data2_i    (Data_Memory.data_o),
     .select_i   (Control.MemtoReg_o),
     .data_o     ()
 );
