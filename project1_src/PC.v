c07728825565fff8059ddc5345bb7580eac4bfd7
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-12-04 15:29:22 +0800

    check for x

--- project1_src/PC.v
+++ project1_src/PC.v
@@ -19,17 +19,20 @@ output  [31:0]      pc_o;
 // Wires & Registers
 reg     [31:0]      pc_o;
 
+//reg                 stall;
+
 
 always@(posedge clk_i or negedge rst_i) begin
     if(~rst_i) begin
         pc_o <= 32'b0;
     end
     else begin
-        if(start_i & ~stall_i)
+        if(start_i & ((stall_i === 1'bx) | (stall_i === 0)))
             pc_o <= pc_i;
         else
             pc_o <= pc_o;
     end
+    // ((stall_i === 1'bx) | (stall_i === 0))
 end
 
 endmodule
