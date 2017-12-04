b13dac935c94d6be6eeb158d7ee67f00462e419f
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-12-04 11:04:31 +0800

    fix hazard

--- project1_src/Hazard_Detection.v
+++ project1_src/Hazard_Detection.v
@@ -11,7 +11,7 @@ input			ID_EX_MemRead_i ;
 input	[4:0]	ID_EX_Rt_i ;
 input	[4:0]	IF_ID_Rs_i ;
 input	[4:0]	IF_ID_Rt_i ;
-input			stall_o ;
+output			stall_o ;
 
 assign stall_o = ID_EX_MemRead_i & 
 				((ID_EX_Rt_i == IF_ID_Rs_i) | (ID_EX_Rt_i == IF_ID_Rt_i)) ;
